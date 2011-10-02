a0c52b2147894e823833acd8e408ab67de70db61
Ingar Shu <ingar@isnd.net>
2011-10-02 13:40:01 -0700

    Fixed typo in build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 coffee=`dirname $0`/node_modules/.bin/coffee
 echo Using coffee executable: $coffee
-$coffee -o build -c coffee/server && coffee -o public/javascripts/build -c coffee/public
+$coffee -o build -c coffee/server && $coffee -o public/javascripts/build -c coffee/public
